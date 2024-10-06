287800f2d602048ec8414c5f70669b5bcab88b98
transgene <geneseth.sys@gmail.com>
2024-10-06 21:16:30 +0200

    Exercises 1 and 2

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,10 @@
+import os
+import pathlib
+import zipfile
 import requests
+import asyncio
+import aiohttp
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,10 +17,74 @@ download_uris = [
 ]
 
 
-def main():
-    # your code here
-    pass
+async def main():
+    os.makedirs("downloads", exist_ok=True)
+    os.chdir("downloads")
+
+    print("Downloading the data... ")
+    failed_urls = []
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = {
+            executor.submit(download_with_requests, url): url for url in download_uris
+        }
+        for future in as_completed(futures):
+            try:
+                if future.result():
+                    failed_urls.append(future.result())
+            except Exception as ex:
+                print(f"Error while downloading data: {ex}")
+    print("done.")
+    if not failed_urls:
+        print("All URLs processed successfully!")
+    else:
+        print(f"{len(failed_urls)} URL(s) weren't processed because of errors:")
+        [
+            print(f"\t{entry[0]}: status {entry[1]}, reason '{entry[2]}'")
+            for entry in failed_urls
+        ]
+
+    print("Extracting the data... ")
+    for entry in pathlib.Path(os.getcwd()).iterdir():
+        if entry.is_file() and entry.suffix == ".zip":
+            try:
+                with zipfile.ZipFile(entry.name) as zip_ref:
+                    zip_ref.extractall(".")
+            except Exception as ex:
+                print(f"Couldn't extract the data from {entry.name}! The error is {ex}")
+
+            try:
+                entry.unlink()
+            except Exception as ex:
+                print(f"Couldn't delete the {entry.name} archive! The error is {ex}")
+    print("done.")
+
+
+def download_with_requests(url) -> tuple[str, int, str]:
+    print(f"Downloading {url}... ")
+    fname = url.split("/")[-1]
+    if os.path.exists(fname):
+        return
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        with open(fname, mode="x+b") as file:
+            file.write(resp.content)
+    else:
+        return url, resp.status_code, resp.text
+
+
+async def download_async(url) -> tuple[str, int, str]:
+    print(f"Downloading {url}... ")
+    fname = url.split("/")[-1]
+    if os.path.exists(fname):
+        return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                with open(fname, mode="x+b") as file:
+                    file.write(await resp.read())
+            else:
+                return (url, resp.status, await resp.text())
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,10 +1,55 @@
+import io
+import os
 import requests
 import pandas
+import bs4
+from urllib.parse import urlparse
 
 
 def main():
-    # your code here
-    pass
+    csv_link = find_csv_link("https://catalog.data.gov/dataset/air-quality")
+    dl_result = download_csv(csv_link)
+    if isinstance(dl_result, tuple):
+        print(
+            f"Failed to download {dl_result[0]} with status {dl_result[1]}. Error is {dl_result[2]}"
+        )
+        return
+    df = pandas.read_csv(dl_result)
+    biggest_vals = df.nlargest(5, "Data Value")
+    print("Top 5 biggest values:")
+    for i, row in enumerate(biggest_vals.itertuples(index=False), start=1):
+        print(f"{i}. {row._10}: measured on {row.Start_Date} in {row._7}")
+
+
+def find_csv_link(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise Exception(
+            f"Can't download the page! Status: {resp.status_code}, error: {resp.text}"
+        )
+    soup = bs4.BeautifulSoup(resp.text, "html.parser")
+    csv_links = soup.find_all("a", title="Comma Separated Values File")
+    if len(csv_links) == 0:
+        raise Exception("Can't find the CSV link")
+    if len(csv_links) > 1:
+        raise Exception("More than one CSV links")
+    btn_div = csv_links[0].parent.find("div", attrs={"class": "btn-group"})
+    if btn_div is None:
+        raise Exception("Can't find the CSV download button")
+    csv_link = btn_div.find("a", attrs={"data-format": "csv"})["href"]
+    return csv_link
+
+
+def download_csv(url) -> tuple[str, int, str] | str:
+    print(f"Downloading {url}... ")
+    fname = urlparse(url).path.split("/")[-1]
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        with open(fname, mode="w+b") as file:
+            file.write(resp.content)
+        return fname
+    else:
+        return url, resp.status_code, resp.text
 
 
 if __name__ == "__main__":
