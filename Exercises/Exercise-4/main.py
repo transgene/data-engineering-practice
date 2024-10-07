b635eae1f2b0273d64fd3c05d7aab19957b7e5da
transgene <geneseth.sys@gmail.com>
2024-10-07 22:02:06 +0200

    started ex. 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,25 @@
-import boto3
+import csv
+import glob
+import json
+import os
+import pathlib
 
 
 def main():
-    # your code here
-    pass
+    converted_dir = pathlib.Path("converted")
+    if converted_dir.exists():
+        os.removedirs(converted_dir)
+
+    json_paths = glob.glob("data/**/*.json", recursive=True)
+
+    converted_dir.mkdir()
+    for json_path in json_paths:
+        with open(json_path) as json_file:
+            data = json.load(json_file)
+            # with open(f"converted/{pathlib.Path(json_path).stem}.csv", "w") as csv_file:
+            #     writer = csv.writer(csv_file)
+            #     for row in data:
+            #         writer.writerow(row)
 
 
 if __name__ == "__main__":
